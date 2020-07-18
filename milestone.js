797d87f8a4503bc870a85d80aa2410cc12a35033
jewelran <mdjewelrana6487@gmail.com>
2020-07-18 20:34:04 +0600

    milestonejs

---
+++ milestone.js
@@ -0,0 +1,16 @@
+// feetToMile-----------
+ 
+ function feetToMile(feet){
+     var mile = feet / 5280;
+     return mile;
+ }
+ var anyFeet = [5000]
+ var avarageMile = feetToMile(anyFeet [0])
+ console.log(avarageMile);
+
+// others wey------
+var feet = 5000;
+var mile = feet / 5280;
+console.log(mile);
+
+
